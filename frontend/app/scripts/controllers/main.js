9ec51fb0d61a4956062e21ff9dc3e82ac54cfe3e
Cris Araya <araya.luster@gmail.com>
2013-08-18 22:16:32 -0600

    loading ready for main controller

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('wamsApp')
-  .controller('MainCtrl', ['$scope', '$route', 
-  	function ($scope, $route) {
+  .controller('MainCtrl', ['$scope', '$route', 'appLoading', 
+  	function ($scope, $route, appLoading) {
   		$scope.$route = $route;
+  		appLoading.ready();
 	  }
   ]);
